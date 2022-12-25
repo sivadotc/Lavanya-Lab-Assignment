854f241aa95ffcd2e995d35c0ca82a329e97b23a
Siva <sivadotpc@gmail.com>
2022-12-25 09:29:40 +0530

    Initial commit

---
+++ app/src/main/java/com/example/ramyalabassignment/ui/theme/Color.kt
@@ -0,0 +1,11 @@
+package com.example.ramyalabassignment.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val TextWhite = Color(0xffeeeeee)
+val DeepBlue = Color(0xff06164c)
+val ButtonBlue = Color(0xff505cf3)
+val DarkerButtonBlue = Color(0xff566894)
+val LightRed = Color(0xfffc879a)
+val LightGreen1 = Color(0xff54e1b6)
+val Brown = Color(0xff520120)
